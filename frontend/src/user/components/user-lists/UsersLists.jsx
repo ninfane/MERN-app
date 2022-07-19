95d9ad42490e8fba593645200b2dea7f7e3e6b4e
Marina Crespo <marina.crespo@valtech.com>
2022-07-19 10:29:03 -0300

    general routing

---
+++ frontend/src/user/components/user-lists/UsersLists.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container } from "./styled";
+import UserItem from "../user-item/UserItem";
+
+const UsersLists = (props) => {
+    if (props.items.length === 0) {
+        return (
+            <Container>
+                <h2>No users found</h2>
+            </Container>
+        );
+    }
+    return (
+        <ul>
+            {props.items.map((user) => (
+                <UserItem
+                    key={user.id}
+                    id={id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.places}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default UsersLists;
