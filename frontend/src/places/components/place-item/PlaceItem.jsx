c68aab058ad3000c765f75f081e27f523e74f75e
Marina Crespo <marina.crespo@valtech.com>
2022-07-20 12:16:04 -0300

    added modal lib

--- frontend/src/places/components/place-item/PlaceItem.jsx
+++ frontend/src/places/components/place-item/PlaceItem.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { PlaceCard, BtnContainer } from "../place-item/styled";
+import Modal from "react-modal";
 
 const PlaceItem = (props) => {
+    const customStyles = {
+        content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+        },
+    };
+    const [isOpen, setIsOpen] = useState(false);
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div>
             <PlaceCard>
@@ -18,7 +37,17 @@ const PlaceItem = (props) => {
                     <p>{props.description}</p>
                 </div>
                 <BtnContainer>
-                    <button>MAP</button>
+                    <button id="map" onClick={openModal}>
+                        MAP
+                    </button>
+                    <Modal
+                        isOpen={isOpen}
+                        onRequestClose={closeModal}
+                        contentLabel="Example modal"
+                        style={customStyles}
+                    >
+                        <div onClick={closeModal}>I'm a modal</div>
+                    </Modal>
                     <button>EDIT</button>
                     <button>DELETE</button>
                 </BtnContainer>
